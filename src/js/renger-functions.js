b8b3684176e44ee8e2d68f43071959ff7aee55bb
misselzabet <lisa.kovalevskaya1999@gmail.com>
2024-11-13 14:19:35 +0100

    commit-chsnge

---
+++ src/js/renger-functions.js
@@ -0,0 +1,48 @@
+// render-functions.js
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox;
+
+
+export function renderImages(images) {
+  const galleryContainer = document.querySelector('.gallery');
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <div class="photo-card">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${likes}</p>
+          <p><b>Views:</b> ${views}</p>
+          <p><b>Comments:</b> ${comments}</p>
+          <p><b>Downloads:</b> ${downloads}</p>
+        </div>
+      </div>`
+    )
+    .join('');
+  galleryContainer.innerHTML = markup;
+  refreshLightbox();
+}
+
+export function clearGallery() {
+  document.querySelector('.gallery').innerHTML = '';
+}
+
+function refreshLightbox() {
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  } else {
+    lightbox.refresh();
+  }
+}
